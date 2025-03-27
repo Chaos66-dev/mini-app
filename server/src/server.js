91973b5b014128ec811581aa4d951773a372ca82
Chaoss66-dev <erik.voss@spaceforce.mil>
2025-03-27 09:29:44 -0700

    containerized server and pg talking to each other

--- server/src/server.js
+++ server/src/server.js
@@ -2,9 +2,20 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV||'development']);
 
 server.get('/', (req, res) => {
     res.status(200).send('I am working')
+})
+
+server.get('/movies', async (req, res) => {
+    try {
+        const query = await knex('movies').select("*")
+        res.status(200).json(query)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Internal Server Error'})
+    }
 })
 
 module.exports = server
